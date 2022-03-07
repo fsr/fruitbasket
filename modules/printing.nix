13a39543f98f5a851319c37b8d9bcf47046d045f
revol-xut <revol-xut@protonmail.com>
2022-03-07 15:43:40 +0100

    added Heiko 2

--- modules/printing.nix
+++ modules/printing.nix
@@ -20,7 +20,14 @@
   hardware.printers.ensurePrinters = [
      {
        description = "Drucker im FSR Buero";
-       deviceUri = "";
+       deviceUri = "dnssd://Kyocera%20ECOSYS%20M6630cidn._ipp._tcp.local/?uuid=4509a320-007e-002c-00dd-002507504ad0";
+       location = "FSR Buero";
+       model = "Kyocera ECOSYS M6630cidn KPDL";
+       name = "Heiko";
+     }
+     {
+       description = "Drucker im FSR Buero";
+       deviceUri = "dnssd://Kyocera%20ECOSYS%20M6630cidn._pdl-datastream._tcp.local/?uuid=4509a320-007e-002c-00dd-002507504ad0";
        location = "FSR Buero";
        model = "Kyocera ECOSYS M6630cidn KPDL";
        name = "Heiko";
