ea8e8ae9669d66af147cf7ccce0847560145e1c0
revol-xut <revol-xut@protonmail.com>
2022-09-22 10:36:34 +0200

    playing wiht streaming seutp

---
+++ modules/stream.nix
@@ -0,0 +1,38 @@
+{pkgs, lib, config, ...}: 
+let 
+  website = fetchFromGithub {
+    owner = "fsr";
+    repo = "ese20-ansible";
+    rev = "1b380f3bfd48aae2a17aefbbdd0538f09b7d3bcf";
+    sha256 = "";
+  };
+in {
+  services = {
+    nginx = {
+      virtualHosts = {
+        "stream-frontend.quitte.tassilo-tanneberger.de" = {
+            enableACME = true;
+            forceSSL = true;
+            locations."/" = {
+              root = "${website}/roles/stream_frontend/";
+              proxyWebsockets = true;
+            };
+          };
+          "owncast.quitte.tassilo-tanneberger.de" = {
+            enableACME = true;
+            forceSSL = true;
+            locations."/" = let 
+              cfg = config.services.owncast;  
+            in {
+              proxyPass = "http://${toString cfg.listen}:${toString cfg.post}";
+              proxyWebsockets = true;
+            };
+          };
+        };
+        };
+      };
+      owncast = {
+        enable = true;
+      };
+  };
+}
