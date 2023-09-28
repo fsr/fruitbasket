a2f49374e7918b3af6557c28841f0fb8da4683b1
Rouven Seifert <rouven@rfive.de>
2023-09-28 13:33:18 +0200

    remove ldap test script

--- test.sh
+++
@@ -1,2 +0,0 @@
-ldapsearch -o ldif-wrap=no -x -D "uid=search,ou=users,dc=ifsr,dc=de" -w $(cat /run/secrets/portunus/search-password) '(&(objectClass=posixAccount)(uid='rouven.seifert'))' 'sshPublicKey' -b "ou=users,dc=ifsr,dc=de" \
-| awk '/^sshPublicKey/{$1=""; p=1} /^$/{p=0} {printf p?$0:""}'
