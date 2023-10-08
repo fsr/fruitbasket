3d189694718e979acf9e88d942bf14fee1a89cd5
Rouven Seifert <rouven@rfive.de>
2023-10-08 13:43:04 +0200

    nginx: disable ip anonymizing

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -9,21 +9,21 @@
     recommendedOptimisation = true;
     recommendedTlsSettings = true;
 
-    appendHttpConfig = ''
-      map $remote_addr $remote_addr_anon {
-               ~(?P<ip>\d+\.\d+\.\d+)\.    $ip.0;
-               ~(?P<ip>[^:]+:[^:]+):       $ip::;
-               # IP addresses to not anonymize
-               127.0.0.1                   $remote_addr;
-               ::1                         $remote_addr;
-               default                     0.0.0.0;
-      }
-      log_format  anon_ip   '$remote_addr_anon - $remote_user [$time_local] "$request" '
-                            '$status $body_bytes_sent "$http_referer" '
-                            '"$http_user_agent" "$http_x_forwarded_for"';
+    # appendHttpConfig = ''
+    #   map $remote_addr $remote_addr_anon {
+    #            ~(?P<ip>\d+\.\d+\.\d+)\.    $ip.0;
+    #            ~(?P<ip>[^:]+:[^:]+):       $ip::;
+    #            # IP addresses to not anonymize
+    #            127.0.0.1                   $remote_addr;
+    #            ::1                         $remote_addr;
+    #            default                     0.0.0.0;
+    #   }
+    #   log_format  anon_ip   '$remote_addr_anon - $remote_user [$time_local] "$request" '
+    #                         '$status $body_bytes_sent "$http_referer" '
+    #                         '"$http_user_agent" "$http_x_forwarded_for"';
 
-      access_log  /var/log/nginx/access.log  anon_ip;
-    '';
+    #   access_log  /var/log/nginx/access.log  anon_ip;
+    # '';
   };
   security.acme = {
     acceptTerms = true;
