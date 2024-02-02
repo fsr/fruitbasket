e198002d60ac14a6486e3785f3d7a958bf9150a0
Rouven Seifert <rouven@rfive.de>
2024-02-02 21:41:44 +0100

    mysql: enable backups

--- modules/mysql.nix
+++ modules/mysql.nix
@@ -5,4 +5,16 @@
     package = pkgs.mariadb;
     settings.mysqld.bind_address = "127.0.0.1";
   };
+  services.mysqlBackup = {
+    enable = true;
+    user = "mysql";
+    location = "/var/lib/backup/mysql";
+    databases = [
+      "decisions"
+      "fsrewsp"
+      "nightline"
+      "wiki_ese"
+      "wiki_vernetzung"
+    ];
+  };
 }
