454394981ef5aaf8bd0461ce54b8f8176ea7679f
Jonas Gaffke <jonas@jonasga.io>
2024-02-02 20:26:37 +0100

    quitte: add nightline and fsrewsp sites

---
+++ modules/fsrewsp.nix
@@ -0,0 +1,68 @@
+{ pkgs, config, lib, ... }:
+let
+  domain = "fsrewsp.de";
+  user = "fsrewsp";
+  group = "fsrewsp";
+in
+{
+  users.users.${user} = {
+    group = group;
+    isSystemUser = true;
+  };
+  users.groups.${group} = { };
+
+  services.phpfpm.pools.fsrewsp = {
+    user = "fsrewsp";
+    group = "fsrewsp";
+    settings = {
+      "listen.owner" = config.services.nginx.user;
+      "pm" = "dynamic";
+      "pm.max_children" = 32;
+      "pm.max_requests" = 500;
+      "pm.start_servers" = 2;
+      "pm.min_spare_servers" = 2;
+      "pm.max_spare_servers" = 5;
+      "php_admin_value[error_log]" = "stderr";
+      "php_admin_flag[log_errors]" = true;
+      "catch_workers_output" = true;
+    };
+    phpEnv."PATH" = lib.makeBinPath [ pkgs.php ];
+  };
+
+
+
+  services.nginx.enable = true;
+  services.nginx = {
+    virtualHosts."${domain}" = {
+      addSSL = true;
+      enableACME = true;
+      root = "/srv/web/fsrewsp";
+      extraConfig = ''
+        index index.php index.html;
+      '';
+
+      locations = {
+        "/" = {
+          tryFiles = "$uri $uri/ /index.php?$args";
+        };
+        "~ \.php$" = {
+          extraConfig = ''
+            try_files $uri =404;
+            fastcgi_pass unix:${config.services.phpfpm.pools.fsrewsp.socket};
+            fastcgi_split_path_info ^(.+\.php)(/.+)$;
+            fastcgi_index index.php;
+            include ${pkgs.nginx}/conf/fastcgi_params;
+            include ${pkgs.nginx}/conf/fastcgi.conf;
+            fastcgi_param SCRIPT_FILENAME $document_root/$fastcgi_script_name;
+          '';
+        };
+        "~ \.log$".return = "403";
+        "~ ^/\.user\.ini".return = "403";
+        "~* \.(js|css|png|jpg|jpeg|gif|ico)$".extraConfig = ''
+          expires max;
+          log_not_found off;
+        '';
+      };
+    };
+  };
+}
