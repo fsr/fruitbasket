7f70ae990cb46cc4c777056b2fdc0d75707bb7a1
Fugi <me@fugi.dev>
2024-02-27 14:06:06 +0100

    ftp: custom 403 error page

--- modules/ftp.nix
+++ modules/ftp.nix
@@ -11,12 +11,26 @@ in
     extraConfig = ''
       fancyindex on;
       fancyindex_exact_size off;
+      error_page 403 /403.html;
     '';
     locations."~/(klausuren|uebungen|skripte|abschlussarbeiten)".extraConfig = ''
       allow 141.30.0.0/16;
       allow 141.76.0.0/16;
       deny all;
     '';
-
+    locations."=/403.html" = {
+      root = pkgs.writeTextDir "403.html" ''
+        <html>
+          <head>
+            <title>403 Forbidden</title>
+          </head>
+          <body>
+            <center><h1>403 Forbidden</h1></center>
+            <center>Dieser Ordner ist nur aus dem Uni-Netz zug&aumlnglich.</center>
+            <center>This directory is only accessible from the TUD network.</center>
+          </body>
+        </html>
+      '';
+    };
   };
 }
