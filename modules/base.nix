4f5148fbf4a1f96995b6880ae6a8285aada6ecc9
Fugi <me@fugi.dev>
2024-02-29 12:03:02 +0100

    Show warning when kernel version changes

--- modules/base.nix
+++ modules/base.nix
@@ -11,6 +11,14 @@
       echo System package diff:
       ${config.nix.package}/bin/nix store diff-closures /run/current-system $systemConfig || true
     fi
+
+    NO_FORMAT="\033[0m"
+    F_BOLD="\033[1m"
+    C_RED="\033[38;5;9m"
+    ${pkgs.diffutils}/bin/cmp --silent \
+      <(readlink /run/booted-system/{initrd,kernel,kernel-modules}) \
+      <(readlink /run/current-system/{initrd,kernel,kernel-modules}) \
+      || echo -e "''${F_BOLD}''${C_RED}Kernel version changed, reboot is advised.''${NO_FORMAT}"
   '';
 
   # Select internationalisation properties.
