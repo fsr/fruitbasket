c04e11a958422971561ddd06bbdf310a825db2a1
Rouven Seifert <rouven@rfive.de>
2024-03-06 20:49:02 +0100

    mailman: add robots.txt

--- modules/mailman.nix
+++ modules/mailman.nix
@@ -57,5 +57,11 @@
   services.nginx.virtualHosts."lists.${config.networking.domain}" = {
     enableACME = true;
     forceSSL = true;
+    locations."/robots.txt" = {
+      extraConfig = ''
+        add_header  Content-Type  text/plain;
+        return 200 "User-agent: *\nDisallow: /\n";
+      '';
+    };
   };
 }
