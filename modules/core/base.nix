ddd2514cdb94b64ee60e502f714f48f585656c03
Rouven Seifert <rouven@rfive.de>
2024-03-19 11:30:00 +0100

    activation script: fix kernel warning

--- modules/core/base.nix
+++ modules/core/base.nix
@@ -16,8 +16,8 @@
     F_BOLD="\033[1m"
     C_RED="\033[38;5;9m"
     ${pkgs.diffutils}/bin/cmp --silent \
-      <(readlink /run/booted-system/{initrd,kernel,kernel-modules}) \
       <(readlink /run/current-system/{initrd,kernel,kernel-modules}) \
+      <(readlink $systemConfig/{initrd,kernel,kernel-modules}) \
       || echo -e "''${F_BOLD}''${C_RED}Kernel version changed, reboot is advised.''${NO_FORMAT}"
   '';
 
