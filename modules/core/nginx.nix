8606e89c031ce5433806cafa519184b28174bbab
Jonas Gaffke <jonas@jonasga.io>
2024-04-09 18:38:59 +0200

    nginx: disable http3 to prevent wordpress error

--- modules/core/nginx.nix
+++ modules/core/nginx.nix
@@ -7,14 +7,10 @@
         ({ name, ... }: {
           enableACME = true;
           forceSSL = true;
-          # enable http3 for all hosts
-          quic = true;
-          http3 = true;
           # split up nginx access logs per vhost
           extraConfig = ''
             access_log /var/log/nginx/${name}_access.log;
             error_log /var/log/nginx/${name}_error.log;
-            add_header Alt-Svc 'h3=":443"; ma=86400';
           '';
         })
       );
@@ -26,7 +22,6 @@
     networking.firewall.allowedUDPPorts = [ 443 ];
     services.nginx = {
       enable = true;
-      package = pkgs.nginxQuic;
       additionalModules = [ pkgs.nginxModules.pam ];
       recommendedProxySettings = true;
       recommendedGzipSettings = true;
