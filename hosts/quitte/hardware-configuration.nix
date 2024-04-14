83db5399d7f3cd172a0bd43545b98e6c6a08f25d
Rouven Seifert <rouven@rfive.de>
2024-04-14 11:40:00 +0200

    quitte: format hardware config

--- hosts/quitte/hardware-configuration.nix
+++ hosts/quitte/hardware-configuration.nix
@@ -10,48 +10,41 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    {
-      device = "rpool/nixos/root";
-      fsType = "zfs";
-    };
+  fileSystems."/" = {
+    device = "rpool/nixos/root";
+    fsType = "zfs";
+  };
 
-  fileSystems."/home" =
-    {
-      device = "rpool/nixos/home";
-      fsType = "zfs";
-    };
+  fileSystems."/home" = {
+    device = "rpool/nixos/home";
+    fsType = "zfs";
+  };
 
-  fileSystems."/nix" =
-    {
-      device = "rpool/nixos/nixnew";
-      fsType = "zfs";
-    };
+  fileSystems."/nix" = {
+    device = "rpool/nixos/nixnew";
+    fsType = "zfs";
+  };
 
-  fileSystems."/var/lib" =
-    {
-      device = "rpool/nixos/var/lib";
-      fsType = "zfs";
-    };
+  fileSystems."/var/lib" = {
+    device = "rpool/nixos/var/lib";
+    fsType = "zfs";
+  };
 
-  fileSystems."/var/log" =
-    {
-      device = "rpool/nixos/var/log";
-      fsType = "zfs";
-    };
+  fileSystems."/var/log" = {
+    device = "rpool/nixos/var/log";
+    fsType = "zfs";
+  };
 
-  fileSystems."/boot" =
-    {
-      device = "/dev/disk/by-uuid/3278-8D00";
-      fsType = "vfat";
-      options = [ "nofail" ];
-    };
-  fileSystems."/boot2" =
-    {
-      device = "/dev/disk/by-uuid/3366-F71E";
-      fsType = "vfat";
-      options = [ "nofail" ];
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/3278-8D00";
+    fsType = "vfat";
+    options = [ "nofail" ];
+  };
+  fileSystems."/boot2" = {
+    device = "/dev/disk/by-uuid/3366-F71E";
+    fsType = "vfat";
+    options = [ "nofail" ];
+  };
 
   swapDevices = [ ];
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
