3f47b32983b7f12cc3985196897dd061048f42c5
Joachim Stramke <joachim.stramke@ifsr.de>
2024-04-29 15:12:52 +0200

    add first node

--- modules/monitoring.nix
+++ modules/monitoring.nix
@@ -39,6 +39,21 @@ in
   services.prometheus = {
     enable = true;
     port = 9001;
+    exporters = {
+      node = {
+        enable = true;
+        enabledCollectors = [ "systemd" ];
+        port = 9002;
+      };
+    };
+    scrapeConfigs = [
+      {
+        job_name = "node";
+        static_configs = [{
+          targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
+        }];
+      }
+    ];
   };
 
   # nginx reverse proxy
