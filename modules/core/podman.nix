68138c0a31c886e4979bba4b14008fe5b7640f00
quitte <root@quitte>
2024-05-19 11:34:46 +0200

    podman: add dive and podman-tui

--- modules/core/podman.nix
+++ modules/core/podman.nix
@@ -14,4 +14,13 @@
     };
   };
   virtualisation.oci-containers.backend = "podman";
+
+
+  # Useful otherdevelopment tools
+  environment.systemPackages = with pkgs; [
+    dive # look into docker image layers
+    podman-tui # status of containers in the terminal
+    #docker-compose # start group of containers for dev
+    #podman-compose # start group of containers for dev
+  ];
 }
