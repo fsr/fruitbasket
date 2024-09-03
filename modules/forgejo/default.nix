382bbc66017ead28469df63bbbc1f36ed1ee58a7
quitte <root@quitte>
2024-09-03 10:17:25 +0200

    forgejo: federation

--- modules/forgejo/default.nix
+++ modules/forgejo/default.nix
@@ -22,15 +22,6 @@ in
 
   services.forgejo = {
     enable = true;
-    # package = pkgs.forgejo.overrideAttrs (_old: {
-    #   # patches = [
-    #   #   # migration fix
-    #   #   (pkgs.fetchpatch {
-    #   #     url = "https://codeberg.org/forgejo/forgejo/commit/ae463c7c559e02975ce5e758d8780def978eebee.patch";
-    #   #     hash = "sha256-cOXPvkLS0n+ynSBTrmEtumZ2PYBeCZmxPpFktqkw6Fo=";
-    #   #   })
-    #   # ];
-    # });
     user = gitUser;
     group = gitUser;
     lfs.enable = true;
@@ -79,6 +70,7 @@ in
         PROVIDER = "db";
       };
       actions.ENABLED = true;
+      federation.ENABLED = true;
     };
   };
 
