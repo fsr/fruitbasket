161a4ae838555e5fddfd52cec98899cfcd543a5d
Fugi <me@fugi.dev>
2024-10-06 16:54:32 +0200

    ese website caching

--- modules/web/ese.nix
+++ modules/web/ese.nix
@@ -6,11 +6,19 @@ in
   services.nginx = {
     virtualHosts."${domain}" = {
       locations."= /" = {
-        return = "301 /2024/";
+        # temporary redirect, to avoid caching problems
+        return = "302 /2024/";
       };
       locations."/" = {
         root = "/srv/web/ese/served";
         tryFiles = "$uri $uri/ =404";
+      };
+      # cache static assets
+      locations."~* \.(?:css|svg|webp|jpg|jpeg|gif|png|ico|mp4|mp3|ogg|ogv|webm|ttf|woff2|woff)$" = {
+        root = "/srv/web/ese/served";
+        extraConfig = ''
+          expires 1y;
+        '';
       };
     };
   };
