01ad4cf730f1d48c29e1c2fd420411b3d8e1a5cf
Jonas Gaffke <jonas@jonasga.io>
2024-12-31 11:36:45 +0100

    kanboard: update

--- modules/kanboard.nix
+++ modules/kanboard.nix
@@ -8,7 +8,7 @@ in
 
   virtualisation.oci-containers = {
     containers.kanboard = {
-      image = "ghcr.io/kanboard/kanboard:v1.2.42";
+      image = "ghcr.io/kanboard/kanboard:v1.2.43";
       volumes = [
         "kanboard_data:/var/www/app/data"
         "kanboard_plugins:/var/www/app/plugins"
