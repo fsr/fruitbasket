4782376b23bb0b18a8bbe0967171b1f303f0a1bd
Frieder Hannenheim <friederhannenheim@riseup.net>
2025-01-30 13:22:04 +0100

    remove redundant forceSSL and enableACME from notenrechner.nix

--- modules/web/notenrechner.nix
+++ modules/web/notenrechner.nix
@@ -2,8 +2,6 @@
   domain = "notenrechner.${config.networking.domain}";
 in {
   services.nginx.virtualHosts."${domain}" = {
-    forceSSL = true;
-    enableACME = true;
     root = specialArgs.notenrechner.packages."x86_64-linux".default;
   };
 }
