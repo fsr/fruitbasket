9c65f5893f4141cf11c65a94c9162f80c0b8fe75
Rouven Seifert <rouven.seifert@ifsr.de>
2026-04-14 12:14:28 +0200

    mailman: remove unneeded overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,20 +1,9 @@
 _final: prev:
 let
-  inherit (prev) fetchurl;
   inherit (prev) libpq;
   inherit (prev) lib;
 in
 {
-  # Mailman internal server error fix
-  # https://gitlab.com/mailman/mailman/-/issues/1137
-  # https://github.com/NixOS/nixpkgs/pull/321136
-  pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-    (_python-final: python-prev: {
-      readme-renderer = python-prev.readme-renderer.overridePythonAttrs (_oldAttrs: {
-        propagatedBuildInputs = [ python-prev.cmarkgfm ];
-      });
-    })
-  ];
   sope = (prev.sope.overrideAttrs (old: {
     patches = [
       ./sope/0001-NGHashMap-keep-root-last-consistent-to-fix-segfault-.patch
