596b16db6f2952fbd0eabf711cb621efea140329
Rouven Seifert <rouven.seifert@ifsr.de>
2026-07-16 22:58:40 +0200

    cache: add nix-community cache
    should speed up authentik compile times

--- hosts/quitte/configuration.nix
+++ hosts/quitte/configuration.nix
@@ -18,6 +18,18 @@
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.forceImportRoot = false;
 
+  nix = {
+    settings = {
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+  };
+
+
   services.zfs = {
     trim.enable = true;
     autoScrub.enable = true;
